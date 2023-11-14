b2912a115ee798131eba3672a6524cda34216443
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2023-11-14 13:47:55 +0900

    [feat[오류수정

--- api.js
+++ api.js
@@ -1,7 +1,8 @@
-const express = require("express");
+const express = require('express');
 const cors = require("cors");
 const app = express();
 const port = 3000;
+
 const path = require("path");
 const functions = require("firebase-functions");
 
